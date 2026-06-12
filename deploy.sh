4608d8b90b2952ba9d15a0944f2d05b1b30a1206
Dayne Myburgh <dayne@valourite.co.za>
2026-06-12 15:52:16 +0200

    Stop tracking Composer vendor files

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cd /home/danks/repositories/DanksAndStrydom
-
-git pull origin main
-
-rm -rf /home/danks/public_html/build
-cp -R /home/danks/repositories/DanksAndStrydom/public/build /home/danks/public_html/build
